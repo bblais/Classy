fixed the backprop
Fixed Backprop using code from
https://github.com/IssamLaradji/NeuralNetworks

I just wrapped that one, and fixed my examples a bit.

classy/autoencoder.py
@@ -9,14 +9,14 @@ import numpy as np
 from scipy.optimize import fmin_l_bfgs_b
 from scipy.linalg import norm
 from itertools import cycle, izip
+from sklearn.utils import check_X_y,check_array
 from sklearn.utils import check_random_state
-from sklearn.utils import check_X_y
 from sklearn.utils import gen_even_slices
 from sklearn.utils import shuffle
 from sklearn.utils.extmath import safe_sparse_dot
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.utils.extmath import logistic_sigmoid, safe_sparse_dot
-
+from sklearn.utils.extmath import safe_sparse_dot
+from base import logistic, softmax, binary_KL_divergence
 
 def _identity(X):
     """returns the same input array."""
@@ -147,7 +147,7 @@ class Autoencoder(BaseEstimator, TransformerMixin):
     """
     def __init__(
         self, n_hidden=25, algorithm='l-bfgs',
-        decoder = 'non_linear', alpha=3e-3, beta=3, sparsity_param=0.1,
+        decoder = 'linear', alpha=3e-3, beta=3, sparsity_param=0.1,
         batch_size=500, shuffle_data=False, max_iter=200, tol=1e-5, learning_rate="constant", eta0=0.5, 
         power_t = 0.25, verbose=False, random_state=None):
             
@@ -167,23 +167,34 @@ class Autoencoder(BaseEstimator, TransformerMixin):
         self.verbose = verbose
         self.random_state = random_state
         
-        self.activation_func = logistic_sigmoid
+        self.activation_func = logistic
         self.derivative_func = _d_logistic
         
     def _init_fit(self, n_features):
         """Initialize weight and bias parameters."""
+
         rng = check_random_state(self.random_state)
+        weight_init_bound1 = np.sqrt(6. / (n_features + self.n_hidden))
+        weight_init_bound2 = np.sqrt(6. / (n_features + self.n_hidden))      
+        rng = check_random_state(self.random_state)
+        self.coef_hidden_ = rng.uniform(-weight_init_bound1, weight_init_bound1, (n_features, self.n_hidden))
+        rng = check_random_state(self.random_state)
+        self.intercept_hidden_ = rng.uniform(-weight_init_bound1, weight_init_bound1, self.n_hidden)
+        rng = check_random_state(self.random_state)
+        self.coef_output_ = rng.uniform(-weight_init_bound2, weight_init_bound2, (self.n_hidden, n_features))
+        rng = check_random_state(self.random_state)
+        self.intercept_output_ = rng.uniform(-weight_init_bound2, weight_init_bound2, n_features)
         
-        self.coef_hidden_ = rng.uniform(-1, 1, (n_features, self.n_hidden))
-        self.coef_output_ = rng.uniform(-1, 1, (self.n_hidden, n_features))
-        self.intercept_hidden_ = rng.uniform(-1, 1, self.n_hidden)
-        self.intercept_output_ = rng.uniform(-1, 1, n_features)
+        #self.coef_hidden_ = np.array([[0.12, 0.13], [0.12, 0.13]])
+        #self.coef_output_ = np.array([[0.12, 0.13], [0.12, 0.13]])
+        #self.intercept_hidden_ = np.array([0.5, 0.4])
+        #self.intercept_output_ = np.array([0.12, 0.6])
 
     def _init_param(self):
         """Sets the activation, derivative, loss and output functions."""      
         # output for non-linear
         if self.decoder=='non_linear':
-            self.output_func =  logistic_sigmoid
+            self.output_func =  logistic
             self.output_derivative = _d_logistic
         # output for linear
         if self.decoder=='linear':
@@ -270,12 +281,12 @@ class Autoencoder(BaseEstimator, TransformerMixin):
         -------
         self
         """
+        X = check_array(X)
         #X = atleast2d_or_csr(X, dtype=np.float64, order="C")
         n_samples, n_features = X.shape
         self._init_fit(n_features)
         self._init_param()
         self._init_t_eta_()
-        X,y=check_X_y(X,y, dtype=np.float64, order="C")
         
         if self.shuffle_data:
             X, y = shuffle(X, y, random_state=self.random_state)
@@ -303,7 +314,7 @@ class Autoencoder(BaseEstimator, TransformerMixin):
                     for batch_slice in batch_slices:
                         cost = self.backprop_sgd(
                             X[batch_slice],
-                            n_features, self.batch_size,
+                            n_features, batch_size,
                             delta_o, a_hidden, a_output)
                     if self.verbose:
                         print("Iteration %d, cost = %.2f"
@@ -352,6 +363,7 @@ class Autoencoder(BaseEstimator, TransformerMixin):
        -------
        [1] http://ufldl.stanford.edu/wiki/index.php/Autoencoders_and_Sparsity
         """
+
         # Forward propagate
         a_hidden[:] = self.activation_func(safe_sparse_dot(X, self.coef_hidden_)
                                       + self.intercept_hidden_)
@@ -368,7 +380,7 @@ class Autoencoder(BaseEstimator, TransformerMixin):
         # Backward propagate
         diff = X - a_output
         #Linear decoder
-        delta_o[:] = -diff * self.output_derivative(a_output)
+        delta_o[:] = -diff
 
         delta_h = (
             (safe_sparse_dot(delta_o, self.coef_output_.T) +
@@ -377,7 +389,7 @@ class Autoencoder(BaseEstimator, TransformerMixin):
             
         # Get cost 
         cost = np.sum(diff ** 2) / (2 * n_samples)
-        
+        #print 'cost1', cost
         # Add regularization term to cost 
         cost += (0.5 * self.alpha) * (
             np.sum(self.coef_hidden_ ** 2) + np.sum(

classy/supervised.py
@@ -3,7 +3,6 @@ import pylab as pl
 import utils
 from sklearn.utils import check_X_y,check_array
 from multilayer_perceptron  import MultilayerPerceptronClassifier
-from mlp import MLPClassifier
 from sklearn.utils.extmath import safe_sparse_dot
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression as LogReg
@@ -23,54 +22,14 @@ class SVM(SVC,GenericClassifier):
 class LogisticRegression(LogReg,GenericClassifier):
     pass
 
-class BackProp2(MLPClassifier,GenericClassifier):
-    def __init__(self,**kwargs):
-        if 'n_hidden' not in kwargs:
-            raise ValueError,"Must specify n_hidden"
-    
-        MLPClassifier.__init__(self,**kwargs)
-        self.equivalent={'weights_xh':'weights1_',
-                        'weights_hy':'weights2_',
-                        'bias_h':'bias1_',
-                        'bias_y':'bias2_',
-                        }
-        self.shuffle_data=True
-        self.max_epochs=50
-
-    def fit(self,*args,**kwargs):
-        if 'max_epochs' not in kwargs:
-            max_epochs=self.max_epochs
-
-        self.max_epochs=max_epochs
-            
-        MLPClassifier.fit(self,*args,
-            max_epochs=self.max_epochs,
-            shuffle_data=self.shuffle_data,
-            **kwargs)
-        for name in self.equivalent:
-            super(MLPClassifier,self).__setattr__(name,self.__getattribute__(self.equivalent[name]))
-    
-
-    def output(self, X):
-        n_samples = X.shape[0]
-        x_hidden = np.empty((n_samples, self.n_hidden))
-        x_output = np.empty((n_samples, self.n_outs))
-        self._forward(None, X, slice(0, n_samples), x_hidden, x_output)
-        return x_hidden,x_output
-
-    
+   
 class BackProp(MultilayerPerceptronClassifier,GenericClassifier):
     def __init__(self,**kwargs):
-        if 'n_hidden' not in kwargs:
-            raise ValueError,"Must specify n_hidden"
         if 'tol' not in kwargs:
             kwargs['tol']=1e-7
     
         MultilayerPerceptronClassifier.__init__(self,**kwargs)
-        self.equivalent={'weights_xh':'coef_hidden_',
-                        'weights_hy':'coef_output_',
-                        'bias_h':'intercept_hidden_',
-                        'bias_y':'intercept_output_',
+        self.equivalent={
                         }
         
     def fit(self,*args,**kwargs):
@@ -92,16 +51,30 @@ class BackProp(MultilayerPerceptronClassifier,GenericClassifier):
         array, shape (n_samples)
         Predicted target values per element in X.
         """
-        X = check_array(X)
 
-        a_hidden = self.activation_func(safe_sparse_dot(X, self.coef_hidden_) +
-                                        self.intercept_hidden_)
-        output = safe_sparse_dot(a_hidden, self.coef_output_) +\
-            self.intercept_output_
-        if output.shape[1] == 1:
-            output = output.ravel()
+        X = check_array(X, accept_sparse=['csr', 'csc', 'coo'])
 
-        return a_hidden,output
+        # Make sure self.hidden_layer_sizes is a list
+        hidden_layer_sizes = self.hidden_layer_sizes
+        if not hasattr(hidden_layer_sizes, "__iter__"):
+            hidden_layer_sizes = [hidden_layer_sizes]
+        hidden_layer_sizes = list(hidden_layer_sizes)
+
+        layer_units = [X.shape[1]] + hidden_layer_sizes + \
+            [self.n_outputs_]
+
+        # Initialize layers
+        activations = []
+        activations.append(X)
+
+        for i in range(self.n_layers_ - 1):
+            activations.append(np.empty((X.shape[0],
+                                         layer_units[i + 1])))
+        # forward propagate
+        self._forward_pass(activations, with_output_activation=False)
+        y_pred = activations[-1]
+
+        return activations[1:]
 
 
 from sklearn.neighbors import KNeighborsClassifier

classy/unsupervised.py
@@ -1,9 +1,9 @@
-#from sklearn.utils.validation import check_arrays, atleast2d_or_csr, column_or_1d
 from sklearn.utils.extmath import safe_sparse_dot
 import autoencoder
-from sklearn.utils import check_X_y
+from sklearn.utils import check_X_y,check_array
 from autoencoder import Autoencoder as AutoEncoder
 from sklearn.decomposition import RandomizedPCA
+from multilayer_perceptron  import MultilayerPerceptronAutoencoder
 from numpy import array
 from datasets import Struct
 from copy import deepcopy
@@ -41,22 +41,20 @@ class GenericFilter(object):
         return new_data
 
 
-class AutoEncoder(autoencoder.Autoencoder,GenericFilter):
+class AutoEncoder(MultilayerPerceptronAutoencoder,GenericFilter):
 
     def __init__(self,*args,**kwargs):    
-        autoencoder.Autoencoder.__init__(self,*args,**kwargs)
-        self.equivalent={'weights_xh':'coef_hidden_',
-                        'weights_hy':'coef_output_',
-                    }
+        MultilayerPerceptronAutoencoder.__init__(self,*args,**kwargs)
+        self.equivalent={}
     def fit(self,*args,**kwargs):
-        autoencoder.Autoencoder.fit(self,*args,**kwargs)
+        MultilayerPerceptronAutoencoder.fit(self,*args,**kwargs)
         for name in self.equivalent:
-            super(autoencoder.Autoencoder,self).__setattr__(name,self.__getattribute__(self.equivalent[name]))
+            super(MultilayerPerceptronAutoencoder,self).__setattr__(name,self.__getattribute__(self.equivalent[name]))
                         
     def fit_transform(self,*args,**kwargs):
-        result=autoencoder.Autoencoder.fit_transform(self,*args,**kwargs)
+        result=MultilayerPerceptronAutoencoder.fit_transform(self,*args,**kwargs)
         for name in self.equivalent:
-            super(autoencoder.Autoencoder,self).__setattr__(name,self.__getattribute__(self.equivalent[name]))
+            super(MultilayerPerceptronAutoencoder,self).__setattr__(name,self.__getattribute__(self.equivalent[name]))
 
         return result
 
@@ -72,16 +70,30 @@ class AutoEncoder(autoencoder.Autoencoder,GenericFilter):
         array, shape (n_samples)
         Predicted target values per element in X.
         """
-        X = atleast2d_or_csr(X)
+        X = check_array(X, accept_sparse=['csr', 'csc', 'coo'])
 
-        a_hidden = self.activation_func(safe_sparse_dot(X, self.coef_hidden_) +
-                                        self.intercept_hidden_)
-        output = safe_sparse_dot(a_hidden, self.coef_output_) +\
-            self.intercept_output_
-        if output.shape[1] == 1:
-            output = output.ravel()
+        # Make sure self.hidden_layer_sizes is a list
+        hidden_layer_sizes = self.hidden_layer_sizes
+        if not hasattr(hidden_layer_sizes, "__iter__"):
+            hidden_layer_sizes = [hidden_layer_sizes]
+        hidden_layer_sizes = list(hidden_layer_sizes)
 
-        return a_hidden,output
+        layer_units = [X.shape[1]] + hidden_layer_sizes + \
+            [self.n_outputs_]
+
+        # Initialize layers
+        activations = []
+        activations.append(X)
+
+        for i in range(self.n_layers_ - 1):
+            activations.append(np.empty((X.shape[0],
+                                         layer_units[i + 1])))
+        # forward propagate
+        self._forward_pass(activations, with_output_activation=False)
+        y_pred = activations[-1]
+
+        return activations[1:]
+
 
     def plot(self,only=None):
         from pylab import plot,subplot,sqrt,ceil,title